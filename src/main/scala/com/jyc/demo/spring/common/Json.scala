24ab76028f17961fb3edfabc69d8c4a7336f1b91
Nate Buwalda <natebuwalda@gmail.com>
2013-06-05 20:06:25 -0500

    Implemented post

---
+++ src/main/scala/com/jyc/demo/spring/common/Json.scala
@@ -0,0 +1,14 @@
+package com.jyc.demo.spring.common
+
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.databind.ObjectMapper
+
+object Json {
+  val mapper = new ObjectMapper()
+  mapper.registerModule(DefaultScalaModule)
+
+  def toJson[T](value: T): String = mapper.writeValueAsString(value)
+  def fromJson[T](json: String, clazz: Class[T]): T = {
+    mapper.readValue(json, clazz)
+  }
+}
--- src/main/scala/com/jyc/demo/spring/employee/EmployeeController.scala
+++ src/main/scala/com/jyc/demo/spring/employee/EmployeeController.scala
@@ -1,9 +1,11 @@
 package com.jyc.demo.spring.employee
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.{RequestMethod, RequestMapping, ResponseBody}
+import org.springframework.web.bind.annotation.{RequestBody, RequestMethod, RequestMapping, ResponseBody}
 import org.springframework.beans.factory.annotation.Autowired
 import collection.JavaConversions._
+import scala.beans.BeanProperty
+import com.jyc.demo.spring.common.Json
 
 @Controller
 class EmployeeController {
@@ -17,5 +19,21 @@ class EmployeeController {
 
   @RequestMapping(value = Array("/employee"), method = Array(POST))
   @ResponseBody
-  def addNewEmployee() = "post works"
+  def addNewEmployee(@RequestBody rawEmployee: String): ResultMessage = {
+    import Json._
+
+    val insertResult = employeeService.addNewEmployee(fromJson(rawEmployee, classOf[Employee]))
+    insertResult match {
+      case Some((id, doc)) => EmployeeResultMessage("Added obj id %s".format(id), doc)
+      case None => EmployeeErrorMessage("insert failed")
+    }
+  }
 }
+
+sealed trait ResultMessage {
+  val message: String
+}
+case class EmployeeResultMessage(@BeanProperty message: String, @BeanProperty document: Employee) extends ResultMessage
+
+sealed trait ErrorMessage extends ResultMessage
+case class EmployeeErrorMessage(@BeanProperty message: String) extends ErrorMessage
